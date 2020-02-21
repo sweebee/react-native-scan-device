f6e9409b4056a12df24c93e1e3f57b529fc90b06
David Guler <david.guler@mediaburg.ch>
2020-02-21 17:14:57 +0100

    Added JS Wrapper class

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { ScanDevice } = NativeModules;
 
-export default ScanDevice;
+class SubscriptionWrapper
+{
+    constructor(subscription) {
+        this.subscription = subscription;
+    }
+
+    offReceive() {
+        this.subscription.remove();
+    }
+}
+
+class ScanDeviceWrapper
+{
+    constructor() {console.log('constructed')}
+
+    onReceive(callback) {
+        const eventEmitter = new NativeEventEmitter(NativeModules.ScanDevice);
+        const subscription = eventEmitter.addListener('scan/received', callback);
+        return new SubscriptionWrapper(subscription);
+    }
+}
+
+export default new ScanDeviceWrapper();
